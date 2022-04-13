db5a85bcc17649eccb788a3a4cd3eacccb166bc0
seyeeL <406668746@qq.com>
2022-04-13 23:00:20 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# logseq-flomo-plugin
-Sync flomo to logseq
+# Logseq Plugin Template Vue
+
+## Features
+
+- plug & play boilerplate
+- develop with HMR, empowered by lightning fast Vite ⚡
+- windicss for styling
+- pnpm
+
+### How HMR works?
+
+See vite.config.ts. There is a custom plugin that will write a dev only index.html.
+The index.html will
+
+- change its base to `http://${config.server.host}:${config.server.port}`
+- attach `/@vite/client` (allows error overlay and connect HMR ws)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+
+const color = ref("red");
+
+function onClickBackdrop() {
+  logseq.hideMainUI();
+}
+
+</script>
+
+<template>
+  <main
+    @click="onClickBackdrop()"
+    class="
+      backdrop-filter backdrop-blur-md
+      fixed
+      inset-0
+      flex
+      items-center
+      justify-center
+    "
+  >
+    <button @click="color = color === 'red' ? 'green' : 'red'">
+      Color is: {{ color }}
+    </button>
+  </main>
+</template>
+
+<style scoped>
+button {
+  color: v-bind(color);
+}
+</style>
