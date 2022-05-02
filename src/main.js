5c4ac62a0ec8f8c34acfa6babeb0edf35e3f7121
Stanley <swiftwind0405@gmail.com>
2022-05-02 13:29:39 +0800

    feat: add toolbar icon

--- src/main.js
+++ src/main.js
@@ -24,36 +24,13 @@ function main() {
     position: 'fixed',
     zIndex: 11,
   });
-  console.log('main');
-  const key = logseq.baseInfo.id;
-
-  logseq.provideStyle(`
-    @import url("https://at.alicdn.com/t/font_3354931_e10iisbf1yu.css");
-    
-    div[data-injected-ui=open-calendar-${key}] {
-      display: flex;
-      align-items: center;
-      font-weight: 500;
-      position: relative;
-      top: 0px;
-    }
-    
-    div[data-injected-ui=open-calendar-${key}] a {
-      opacity: 1;
-      padding: 6px;
-    }
-    
-    div[data-injected-ui=open-calendar-${key}] iconfont {
-      font-size: 18px;
-    }
-  `);
 
   // external btns
   logseq.App.registerUIItem('toolbar', {
     key: 'open-flomo',
     template: `
       <a class="button" data-on-click="openFlomo">
-        <i class="iconfont icon-flomo"></i>
+        <i class="ti ti-clear-all"></i>
       </a>
     `,
   });
