4d38368f9d1293c4f0c1eb30b1c0c0590f0da839
seyeeL <406668746@qq.com>
2022-05-08 11:56:05 +0800

    fix: modal close

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-modal v-model:visible="visible" :footer="null" :closable="true" destroyOnClose>
+  <a-modal v-model:visible="visible" :footer="null" :closable="true" destroyOnClose @cancel="hideMainUI">
     <template #title>
       <span>Sync Settings</span>
     </template>
